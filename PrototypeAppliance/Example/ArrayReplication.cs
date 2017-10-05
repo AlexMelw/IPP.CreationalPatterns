697a0606b650c7f5ae3809676a565c4b83a02293
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-10-05 14:06:57 +0300

    + Clone() 3 examples

---
+++ PrototypeAppliance/Example/ArrayReplication.cs
@@ -0,0 +1,22 @@
+﻿namespace PrototypeAppliance
+{
+    using System;
+    using EasySharp.NHelpers.CustomExMethods;
+
+    class ArrayReplication
+    {
+        public static void ClonArrayDemo()
+        {
+            int[] array1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int[] array2 = array1.CloneArray();
+
+            Console.Out.WriteLine($"Elements in {nameof(array2)}:");
+
+            foreach (int x in array2)
+            {
+                Console.Out.WriteLine("x = {0}", x);
+            }
+        }
+    }
+}
---
+++ PrototypeAppliance/Program.CodeBehind.cs
@@ -0,0 +1,58 @@
+﻿namespace PrototypeAppliance
+{
+    using System;
+    using System.Collections.Generic;
+    using EasySharp.NHelpers.CustomExMethods;
+    using Example2;
+
+    partial class Program
+    {
+        private static void RunDemo1()
+        {
+            var scrapper = new WebPageScrapper(@"http://utm.md/");
+            var scrapperCopy = (WebPageScrapper) scrapper.Clone();
+
+            scrapper.PrintPageData();
+            scrapperCopy.PrintPageData();
+        }
+
+        private static void RunDemo2()
+        {
+            var tigerTank = new Tank
+            {
+                Mobility = new Mobility
+                {
+                    Power = 900,
+                    TopSpeed = 40
+                },
+                Weaponry = new Weaponry
+                {
+                    Damage = 490,
+                    Penetration = 246
+                },
+                Crew = new List<CrewType>
+                {
+                    CrewType.Commander,
+                    CrewType.Driver,
+                    CrewType.Gunner,
+                    CrewType.Loader,
+                    CrewType.Loader,
+                    CrewType.Radioman
+                }
+            };
+
+            Tank pantherTank = tigerTank.Replicate();
+
+            tigerTank.Mobility.Power = 333;
+            tigerTank.Weaponry.Penetration = 777;
+
+            Console.Out.WriteLine("tigerTank = {0}", tigerTank);
+            Console.Out.WriteLine("pantherTank = {0}", pantherTank);
+        }
+
+        private static void RunDemo3()
+        {
+            ArrayReplication.ClonArrayDemo();
+        }
+    }
+}
